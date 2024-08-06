a8ceabcafeac38bab484004a9c64b6b5b0cb9faf
MaurizioSpinelli <mauriziospinelli@hotmail.com>
2024-08-06 15:46:48 -0300

    Update setup-users.sh

--- setup-users.sh
+++ setup-users.sh
@@ -6,22 +6,14 @@ until curl -s -XGET 'http://localhost:9200' >/dev/null; do
     sleep 5
 done
 
-# Configurar senha para os usuários padrão e capturar a saída
+# Configurar senha para os usuários padrão e definir uma senha específica para o usuário 'elastic'
 echo "Configurando senhas..."
-PASSWORD_OUTPUT=$(/usr/share/elasticsearch/bin/elasticsearch-setup-passwords auto --batch)
 
-# Extrair a senha gerada para o usuário 'elastic'
-ELASTIC_PASSWORD=$(echo "$PASSWORD_OUTPUT" | grep -oP "(?<=PASSWORD elastic  : )\S+")
+# Definir a senha desejada para o usuário 'elastic'
+ELASTIC_PASSWORD="pesquisapdf"
 
-if [ -z "$ELASTIC_PASSWORD" ]; then
-    echo "Falha ao obter a senha do usuário 'elastic'."
-    exit 1
-fi
-
-echo "Senha gerada para o usuário 'elastic': $ELASTIC_PASSWORD"
-
-# Definir uma nova senha específica para o usuário 'elastic' usando a senha gerada
-echo "Definindo nova senha para o usuário 'elastic'"
+# Definir a senha para o usuário 'elastic'
+echo "Definindo senha para o usuário 'elastic'"
 curl -XPOST -u elastic:$ELASTIC_PASSWORD -H "Content-Type: application/json" -d '{
-  "password" : "changeme"
+  "password" : "'$ELASTIC_PASSWORD'"
 }' "http://localhost:9200/_security/user/elastic/_password"
